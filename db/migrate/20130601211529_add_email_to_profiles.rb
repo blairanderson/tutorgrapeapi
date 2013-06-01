630426d1e387c0b3d2bc4f4391b0823cfbffa556
Blair Anderson <blair81@gmail.com>
2013-06-01 15:27:48 -0600

    updated a bad migration file

--- db/migrate/20130601211529_add_email_to_profiles.rb
+++ db/migrate/20130601211529_add_email_to_profiles.rb
@@ -1,7 +1,7 @@
 class AddEmailToProfiles < ActiveRecord::Migration
   def change
     add_column :profiles, :email, :string
-    remove_column :profiles, :corportation, :string
+    remove_column :profiles, :corportation
     add_column :profiles, :corporation, :string
   end
 end
