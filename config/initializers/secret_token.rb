6dd97c63217bfb1b69dd5941cf5d569c62f6a36b
Blair Anderson <blair81@gmail.com>
2013-06-01 08:13:26 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorgrapeapi::Application.config.secret_token = '964dc906a773cbe5b8d0f35114da5396eb65792a1f00d749db14f175b810c5bca4c30f709458b08814ca6751ab403a8637f17e5333287373953e044986baf212'
