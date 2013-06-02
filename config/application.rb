a0f15a47492faabf3803e70b8f2e21538ef71985
Blair Anderson <blair81@gmail.com>
2013-06-02 10:18:42 -0600

    precompiling

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ end
 
 module Tutorgrapeapi
   class Application < Rails::Application
+    
+    # config.assets.paths += ["app/assets/fonts"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -38,6 +41,7 @@ module Tutorgrapeapi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
