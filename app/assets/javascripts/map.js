22a5888e1ed89604f498c4304f2b1e08b00f7b20
Blair Anderson <blair81@gmail.com>
2013-06-01 22:06:03 -0600

    javascript map

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,23 @@
+dojo.require("esri.map");
+dojo.require("esri.arcgis.utils");
+dojo.require("esri.dijit.Legend");
+alert('Loading...');
+var map;
+
+function init(){
+  var webmapid = "d2dcc1bd47d04ef0962276474a63a01f";
+  esri.arcgis.utils.createMap(webmapid, "mapDiv").then(function(response){
+    map = response.map;
+
+    var legend = new esri.dijit.Legend({
+      map:map,
+      layerInfos:(esri.arcgis.utils.getLegendLayers(response))
+    },"legendDiv");
+    legend.startup();
+
+
+  },function(error){alert ('Failed to load map');
+      console.log("Error loading webmap: " & dojo.toJson(error));});
+}
+
+dojo.ready(init);
